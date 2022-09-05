ccf93152624a8e3e9eb5b7c29f48b0b54e3c04a3
Vani Jain <vani.jain_cs19@gla.ac.in>
2022-09-05 18:16:19 +0530

    files

--- Assignment1/src/AssignmentTask3.js
+++ Assignment1/src/AssignmentTask3.js
@@ -1,14 +1,28 @@
-var sum = 0;
-var number1 = parseInt(prompt("Enter a number"));
-function varargs() {
+// let number2=prompt(`Enter a number`);
+// let a:number=parseInt(number2);
+// var arr2[];
+// arr2.push(number2);
+function varargs1() {
     var args = [];
     for (var _i = 0; _i < arguments.length; _i++) {
         args[_i] = arguments[_i];
     }
+    var sum1 = 0;
     for (var i = 0; i < args.length; i++) {
-        sum += args[i];
+        sum1 += args[i];
     }
-    console.log(sum);
+    return sum1;
 }
-varargs(1, 2, 3);
-document.write("My sum" + sum);
+// varargs1(1,2,3);
+var value = prompt("Enter length of arguments");
+var value1 = parseInt(value);
+var y = [];
+for (var i = 0; i < value1; i++) {
+    var mynumber = prompt("Enter number");
+    var num = parseInt(mynumber);
+    y.push(num);
+}
+document.body.style.textAlign = "center";
+document.body.style.color = "white";
+document.body.style.fontSize = "24px";
+document.write("My sum is: " + varargs1.apply(void 0, y));
--- Assignment1/src/AssignmentTask3.ts
+++ Assignment1/src/AssignmentTask3.ts
@@ -1,18 +1,33 @@
-let sum1=0;
-let number2=prompt(`Enter a number`);
+// let number2=prompt(`Enter a number`);
 
-let a:number=parseInt(number2);
-var arr2[];
-arr2.push(number2);
+// let a:number=parseInt(number2);
+// var arr2[];
+// arr2.push(number2);
 
 
 function varargs1(...args:number[]){
+    let sum1=0;
+
     for(let i=0;i<args.length;i++){
         sum1+=args[i];
     }
-    console.log(sum1);
+    return sum1;
 }
 // varargs1(1,2,3);
 
-document.write("My sum"+sum1);
+let value:any=prompt("Enter length of arguments");
+let value1=parseInt(value);
+let y:number[]=[];
+for(let i=0;i<value1;i++){
+    let mynumber:any=prompt("Enter number");
+    let num=parseInt(mynumber);
+    y.push(num);
+}
+document.body.style.textAlign="center";
+document.body.style.color="white";
+document.body.style.fontSize="24px";
+
+document.write("My sum is: "+varargs1(...y));
+
+
 
